a1082809bb2d856e5c5bfd21ffab722a5c3ac0c1
Nicolas Budyn <n.budyn@pm.me>
2019-10-12 11:19:10 +0100

    add study of max(rayleigh) and gumbel distribution

--- extreme_rayleigh.py
+++ extreme_rayleigh.py
@@ -1,5 +1,7 @@
 """
 Plot distribution of n iid Rayleigh distributed variables for various n
+
+Show the asymptotic is a Gumbel distribution
 """
 
 import numpy as np
@@ -13,6 +15,13 @@ def rayleigh_logpdf(x):
 
 def rayleigh_logcdf(x):
     return np.log(1 - np.exp(-(x * x) / 2))
+
+
+def maxrayleigh_logcdf(x, n, sigma):
+    """
+    cdf of max of n iid Rayleigh distribution
+    """
+    return n * rayleigh_logcdf(x / sigma)
 
 
 def maxrayleigh_logpdf(x, n, sigma):
@@ -51,3 +60,90 @@ for n in [10, 100, 1000, 10000, 100000, 1000000]:
     plt.plot(t, np.exp(maxrayleigh_logpdf(t, n, sigma)), label=f"n={n}")
 plt.legend()
 plt.title("distribution of n iid Rayleigh")
+
+#%% The mode of MaxRayleigh increases in sqrt(log(n))
+
+sigma = 1.0
+t = np.linspace(0, 8, 2000)[1:]
+nvect = np.array([100, 1000, 10000, 100000, 200000, 500000, 1000000])
+nvect = 10 ** np.linspace(2, 6, 20)
+modes = np.zeros_like(nvect)
+for k, n in enumerate(nvect):
+    modes[k] = t[np.argmax(np.exp(maxrayleigh_logpdf(t, n, sigma)))]
+
+plt.figure()
+plt.plot(modes, np.sqrt(np.log(nvect)), ".-")
+plt.xlabel("sqrt(log(n))")
+plt.ylabel("mode")
+# plt.gca().set_xscale("log")
+
+
+#%% Convergence of (M-b)/a towards standard Gumbel for one x
+
+sigma = 1.0
+
+
+def rayleigh_cdf(x):
+    return 1 - np.exp(-x ** 2 / (sigma ** 2 * 2))
+
+
+def asymptotic_rayleigh(x, n):
+    a = sigma / np.sqrt(2 * np.log(n))
+    b = sigma * np.sqrt(2 * np.log(n))
+    return rayleigh_cdf(a * x + b) ** n
+
+
+def asymptotic_exponential(x, n):
+    # Exponential distribution
+    return (1 - np.exp(-x) / n) ** n
+
+
+def gumbel_cdf(x):
+    return np.exp(-np.exp(-x))
+
+
+def gumbel_pdf(x):
+    return np.exp(-(x + np.exp(-x)))
+
+
+x = 1.0
+plt.figure()
+nvect = 10 ** np.linspace(1, 8, 20)
+plt.plot(nvect, asymptotic_rayleigh(x, nvect))
+# plt.plot(nvect, asymptotic_exponential(x, nvect))
+plt.axhline(gumbel_cdf(x), color="C1")
+plt.gca().set_xscale("log")
+
+#%% Convergence of (M-b)/a towards standard Gumbel for cdf
+sigma = 1.0
+t = np.linspace(0, 8, 1000)
+plt.figure()
+for n in [10, 100, 1000, 1e5, 1e6, 1e7, 1e8]:
+    a = sigma / np.sqrt(2 * np.log(n))
+    b = sigma * np.sqrt(2 * np.log(n))
+    plt.plot(t, np.exp(maxrayleigh_logcdf(a * t + b, n, sigma)), label=f"n={n}")
+plt.plot(t, gumbel_cdf(t), "k--", label="Gumbel")
+plt.legend()
+
+#%% Convergence of (M-b)/a towards standard Gumbel for pdf
+sigma = 1.0
+t = np.linspace(0, 8, 1000)
+plt.figure()
+for n in [10, 100, 1000, 10000]:
+    a = sigma / np.sqrt(2 * np.log(n))
+    b = sigma * np.sqrt(2 * np.log(n))
+    plt.plot(t, a * np.exp(maxrayleigh_logpdf(a * t + b, n, sigma)), label=f"n={n}")
+plt.plot(t, gumbel_pdf(t), "k--", label="Gumbel")
+plt.legend()
+
+#%% Convergence of M towards Gumbel for pdf
+
+sigma = 1.0
+t = np.linspace(0, 8, 1000)[1:]
+plt.figure()
+for n in [10, 100, 1000, 10000]:
+    a = sigma / np.sqrt(2 * np.log(n))
+    b = sigma * np.sqrt(2 * np.log(n))
+    plt.plot(t, np.exp(maxrayleigh_logpdf(t, n, sigma)), label=f"n={n}")
+    plt.plot(t, stats.gumbel_r(loc=b, scale=a).pdf(t), "k--", label=f"Gumbel n={n}")
+plt.legend()
